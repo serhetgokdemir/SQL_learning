3b293cc10eed2de6e016f422fde1702bc901a9da
Serhet Gökdemir <apocalptica38@gmail.com>
2024-08-12 10:50:14 +0300

    added

---
+++ odev8.sql
@@ -0,0 +1,29 @@
+-- CREATE TABLE employee(
+--	id INTEGER,
+--	name VARCHAR(50),
+--	birthday DATE,
+--	email VARCHAR(100)
+-- );
+
+-- Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
+
+-- UPDATE employee
+-- SET name = 'Serhet',
+-- 	birthday = '2001-11-19',
+--	email = 'serhetgokdemir@gmail.com'
+-- WHERE email = 'fivashov1d@hhs.gov'
+-- RETURNING *;
+
+-- UPDATE employee
+-- SET name = 'Şeyhmus',
+-- birthday = '2002-09-15',
+-- email = 'syoldas@gmail.com'
+-- WHERE email = 'frosenstengel1@comsenz.com'
+-- RETURNING * 
+
+-- UPDATE employee
+-- SET name = 'Nuran',
+-- birthday = '1970-11-11',
+-- email = 'nurang@gmail.com'
+-- WHERE email = 'rcrain4@github.com'
+-- RETURNING *
